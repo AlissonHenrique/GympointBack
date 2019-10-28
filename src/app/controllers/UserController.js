3249b16254b96fc205690994c8042e72b86e82ca
Alisson Henrique <alisson-henri@live.com>
2019-10-28 16:45:08 -0300

    construido login

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,11 @@
+
+
+class UserController {
+  async store(req, res) {
+
+    // const response = await User.create(req.body)
+  }
+
+}
+
+export default new UserController()
---
+++ src/config/database.js
@@ -0,0 +1,17 @@
+
+module.exports = {
+  dialect: 'postgres',
+  host: 'tuffi.db.elephantsql.com',
+  username: 'dbcujzat',
+  password: '3-H8Ddo-iqZj2XVDpc6nAW0fK43AFhPO',
+  database: 'dbcujzat',
+  operatorAliases: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
+  }
+}
+
+
+
