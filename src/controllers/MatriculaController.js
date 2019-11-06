e65ce56ad30166138a63e7ac38bdfebe947044e3
Alisson Henrique <alisson-henri@live.com>
2019-11-06 17:28:14 -0300

    data

--- src/controllers/MatriculaController.js
+++ src/controllers/MatriculaController.js
@@ -1,3 +1,4 @@
+import { startOfToday, parseISO, isBefore } from 'date-fns';
 import Matricula from '../models/Matricula';
 
 class MatriculaController {
@@ -7,6 +8,13 @@ class MatriculaController {
   }
 
   async store(req, res) {
+    const { start_date } = req.body;
+
+    // / verifica se a data é antes da atual
+    const startMes = startOfToday(parseISO(start_date));
+    if (isBefore(startMes, new Date())) {
+      return res.status(400).json({ error: 'Esta data não é permitida' });
+    }
     const response = await Matricula.create(req.body);
     return res.json(response);
   }
